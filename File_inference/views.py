9a0be3be6f4aeeda5e818c764a8b15467593cb97
kaung-htet-myat <kaunghm.09@gmail.com>
2022-12-04 19:48:50 +0630

    feat: predict with model

--- File_inference/views.py
+++ File_inference/views.py
@@ -1,3 +1,6 @@
+import os
+import librosa
+from django.conf import settings
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from rest_framework import views
@@ -6,6 +9,13 @@ from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
 
 from File_inference.forms import UploadFileForm
+from File_inference.utils.inference import (
+    get_device,
+    get_label_map,
+    load_model,
+    get_input_segments,
+    get_result,
+)
 
 
 class IndexView(TemplateView):
@@ -15,11 +25,44 @@ class IndexView(TemplateView):
 class PredictView(views.APIView):
     template_name = 'File_inference/result.html'
     renderer_classes = [TemplateHTMLRenderer]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.device = get_device()
+        model_path = os.path.join(settings.MODEL_DIR, settings.MODEL_FILENAME)
+        self.model = load_model(model_path)
+        self.model.to(self.device)
+
+        _, self.label_map = get_label_map(settings.LABEL_PATH)
+
+        self.sample_length = settings.SAMPLE_LENGTH
+        self.sample_rate = settings.SAMPLE_RATE
+        self.fft_size = settings.FFT_SIZE
+        self.hop_length = settings.HOP_LENGTH
+        self.n_mels = settings.N_MELS
     
     def post(self, request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             try:
-                return Response({'prediction': "Rock", 'file': request.FILES['song_file']}, status=status.HTTP_200_OK)
+                sample, in_rate = librosa.load(request.FILES['song_file'], sr=None)
+
+                input_segments = get_input_segments(
+                                    sample,
+                                    in_rate,
+                                    self.sample_length,
+                                    self.sample_rate,
+                                    self.fft_size,
+                                    self.hop_length,
+                                    self.n_mels,
+                                )
+                input_segments = input_segments.to(self.device)
+
+                predictions = self.model(input_segments)
+                predictions = predictions.to('cpu')
+
+                result = get_result(predictions, self.label_map)
+
+                return Response({'prediction': result, 'file': request.FILES['song_file']}, status=status.HTTP_200_OK)
             except ValueError as err:
                 return Response(str(err), status=status.HTTP_400_BAD_REQUEST)
--- music_classifier_web_api/settings.py
+++ music_classifier_web_api/settings.py
@@ -10,10 +10,22 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Model infos
+MODEL_DIR = os.path.join(BASE_DIR, "models")
+MODEL_FILENAME = "efficientnet_prcnn_melspectrogram_epoch_77_model.pth"
+LABEL_PATH = os.path.join(BASE_DIR, "genre_labels.json")
+SAMPLE_LENGTH = 5
+SAMPLE_RATE = 22050
+FFT_SIZE = 1024
+HOP_LENGTH = 256
+N_MELS = 128
 
 
 # Quick-start development settings - unsuitable for production
