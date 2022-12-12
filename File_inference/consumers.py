3a5c262335440ea7a7442f51156162075afab043
kaung-htet-myat <kaunghm.09@gmail.com>
2022-12-12 19:45:10 +0630

    feat: prediction from stream

--- File_inference/consumers.py
+++ File_inference/consumers.py
@@ -1,6 +1,12 @@
 import os
 import json
+import random
+import scipy
+import librosa
+import audiosegment
+import soundfile as sf
 from io import BytesIO
+import base64
 import numpy as np
 import torch
 import torchaudio
@@ -11,6 +17,8 @@ from File_inference.utils.inference import (
     get_device,
     get_label_map,
     load_model,
+    get_input_segments,
+    get_result,
 )
 
 
@@ -25,6 +33,7 @@ class PredictionConsumer(AsyncWebsocketConsumer):
 
         _, self.label_map = get_label_map(settings.LABEL_PATH)
 
+        self.in_rate = 44100
         self.sample_length = settings.SAMPLE_LENGTH
         self.sample_rate = settings.SAMPLE_RATE
         self.fft_size = settings.FFT_SIZE
@@ -35,29 +44,39 @@ class PredictionConsumer(AsyncWebsocketConsumer):
         await self.accept()
             
     async def disconnect(self, close_code):
-        # await self.channel_layer.group_discard(
-        #     self.room_group_name,
-        #     self.channel_name
-        # )
         pass
 
+    async def predict(self, input):
+        input_segments = get_input_segments(
+                            input,
+                            self.in_rate,
+                            self.sample_length,
+                            self.sample_rate,
+                            self.fft_size,
+                            self.hop_length,
+                            self.n_mels,
+                        )
+        input_segments = input_segments.to(self.device)
+
+        predictions = self.model(input_segments)
+        predictions = predictions.to('cpu')
+
+        result = get_result(predictions, self.label_map)
+        return result
+
     async def process_input(self, bytes_data):
+
         f = BytesIO(bytes_data)
-        print(f)
         s = torchaudio.io.StreamReader(f)
-        s.add_basic_audio_stream(132300)
+        s.add_basic_audio_stream(66150)
         input = torch.concat([chunk[0] for chunk in s.stream()])
-        
-        print(input)
+        input = input.squeeze(-1).numpy()
+        return input
 
     async def receive(self, bytes_data):
-        print(type(bytes_data))
-        print(len(bytes_data))
-        
         try:
-            await self.process_input(bytes_data)
-        except RuntimeError as err:
+            input = await self.process_input(bytes_data)
+            prediction = await self.predict(input)
+            await self.send(text_data=json.dumps({"prediction": str(prediction)}))
+        except RuntimeError as err: 
             print(err)
-
-        prediction = "metal"
-        self.send(text_data=json.dumps({"prediction": prediction}))
